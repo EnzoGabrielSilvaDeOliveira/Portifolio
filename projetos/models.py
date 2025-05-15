5dfcef15ab1820c6e44e0078f30250badf497f46
Enzo Gabriel Silva de Oliveira <enzogabrielsilva0102@gmail.com>
2025-05-15 14:03:27 -0300

    Adicionando pasta Static ao projeto

--- projetos/models.py
+++ projetos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Projeto(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField()
+    imagem = models.ImageField(upload_to='imagens/')
+    link = models.URLField()
+
+    def __str__(self):
+        return self.titulo
+    
