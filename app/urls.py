8fac82410d899bb1841638b12c3d07f9cf8b52c8
Enzo Gabriel Silva de Oliveira <enzogabrielsilva0102@gmail.com>
2025-05-20 15:37:34 -0300

    Adcionando diretorio de imagens no projeto e alterando modelo de Projetos

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from projetos.views import HomePageView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', HomePageView.as_view(), name='home')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+   
--- projetos/models.py
+++ projetos/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Projeto(models.Model):
     titulo = models.CharField(max_length=200)
-    descricao = models.TextField()
+    descricao = models.TextField(blank=True, null=True)
     imagem = models.ImageField(upload_to='imagens/')
     linkProjeto = models.URLField()
-    linkGithub = models.URLField()
+    linkGithub = models.URLField(blank=True, null=True) 
 
     def __str__(self):
         return self.titulo
